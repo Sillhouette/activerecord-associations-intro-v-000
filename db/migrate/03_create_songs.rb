d41c26fb323f41125ec6229efda8c151ff5d4776
Richard Melchior <raustin.melchior@gmail.com>
2018-06-15 03:26:58 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[4.2]
   def change
     create_table :songs do |song|
       song.string :name
-      song.integer  :artist_id
+      song.integer :artist_id
       song.integer :genre_id
     end
   end
